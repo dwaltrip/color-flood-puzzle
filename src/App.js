7ab5606539433f45d2b4ed17c9da48365e02b3d6
Daniel Waltrip <dwaltrip77@gmail.com>
2017-04-24 16:08:27 -0700

    Start creating the component heirarchy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class ColorPicker extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className='color-picker'></div>
+    );
+  }
+}
+
+class Grid extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className='grid-container'></div>
+    );
+  }
+}
+
+class Game extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className='game'>
+        <ColorPicker />
+        <Grid />
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div className='app-container'>
+        <div className='header'>Color Flood</div>
+        <Game />
+      </div>
+    );
   }
 }
 
