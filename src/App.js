5037eeb35285c352e0261af6db1dbf61b2909275
Daniel Waltrip <dwaltrip77@gmail.com>
2017-04-24 16:58:28 -0700

    Render grid with random colors

--- src/App.js
+++ src/App.js
@@ -18,7 +18,15 @@ class Grid extends Component {
   }
   render() {
     return (
-      <div className='grid-container'></div>
+      <div className='grid-container'>
+        {this.props.grid.map(row =>
+          <div className='grid-row'>
+            {row.map(color => (
+              <div className={`grid-square ${color}-square`}/>
+            ))}
+          </div>
+        )}
+      </div>
     );
   }
 }
@@ -26,12 +34,25 @@ class Grid extends Component {
 class Game extends Component {
   constructor() {
     super();
+    this.defaultSize = { width: 20, height: 15 };
+    this.colors = ['green', 'blue', 'orange', 'red', 'yellow', 'purple'];
+    this.setupGrid();
+  }
+  getRandomColor() {
+    var index = randomInt(0, this.colors.length);
+    return this.colors[index];
+  }
+  setupGrid() {
+    var { width, height } = this.defaultSize;
+    this.state = {
+      grid: range(height, ()=> range(width, ()=> this.getRandomColor()))
+    };
   }
   render() {
     return (
-      <div className='game'>
+      <div className='game-container'>
         <ColorPicker />
-        <Grid />
+        <Grid grid={this.state.grid}/>
       </div>
     );
   }
@@ -41,7 +62,13 @@ class App extends Component {
   render() {
     return (
       <div className='app-container'>
-        <div className='header'>Color Flood</div>
+        <div className='header'>
+          <div className='header-text'>Color Flood</div>
+          <div className='buttons'>
+            <button>New Game</button>
+          </div>
+        </div>
+
         <Game />
       </div>
     );
@@ -49,3 +76,12 @@ class App extends Component {
 }
 
 export default App;
+
+function range(size, valueOrFn) {
+  var fn = typeof valueOrFn === 'function' ? valueOrFn : (()=> valueOrFn);
+  return Array.apply(null, Array(size)).map((_, i)=> fn(i));
+}
+
+function randomInt(min, max) {
+  return min + Math.round(Math.random() * (max - 1 - min));
+}
