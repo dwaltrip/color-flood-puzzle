d5218368f443ce66b28fd5f49a6bd90b911eec84
Daniel Waltrip <dwaltrip77@gmail.com>
2017-04-24 18:42:29 -0700

    Implement ColorPicker and naive color flooding

--- src/App.js
+++ src/App.js
@@ -7,7 +7,18 @@ class ColorPicker extends Component {
   }
   render() {
     return (
-      <div className='color-picker'></div>
+      <div className='color-picker'>
+        <label>Choose next flood color:</label>
+        <div className='color-options'>
+          {this.props.colors.map((color, i) =>
+            <div
+              className={`color-option ${color}-bg`}
+              onClick={()=> this.props.onSelect(color)}
+              key={i}
+            />
+          )}
+        </div>
+      </div>
     );
   }
 }
@@ -19,10 +30,10 @@ class Grid extends Component {
   render() {
     return (
       <div className='grid-container'>
-        {this.props.grid.map(row =>
-          <div className='grid-row'>
-            {row.map(color => (
-              <div className={`grid-square ${color}-square`}/>
+        {this.props.grid.map((row, y) =>
+          <div className='grid-row' key={y}>
+            {row.map((color, x) => (
+              <div className={`grid-square ${color}-bg`} key={x}/>
             ))}
           </div>
         )}
@@ -44,14 +55,26 @@ class Game extends Component {
   }
   setupGrid() {
     var { width, height } = this.defaultSize;
-    this.state = {
-      grid: range(height, ()=> range(width, ()=> this.getRandomColor()))
-    };
+    var grid = range(height, ()=> range(width, ()=> this.getRandomColor()));
+    this.state = { grid };
+  }
+  handleColorSelection(colorToFlood) {
+    var grid = this.state.grid;
+    var points = findConnectedSquares(grid, 0, 0);
+    points.forEach(point => {
+      var [x, y] = point;
+      grid[y][x] = colorToFlood;
+    });
+    grid[0][0] = colorToFlood;
+    this.setState({ grid });
   }
   render() {
     return (
       <div className='game-container'>
-        <ColorPicker />
+        <ColorPicker
+          colors={this.colors}
+          onSelect={color => this.handleColorSelection(color)}
+        />
         <Grid grid={this.state.grid}/>
       </div>
     );
@@ -76,6 +99,41 @@ class App extends Component {
 }
 
 export default App;
+
+function findConnectedSquares(grid, startX, startY) {
+  var targetValue = grid[startY][startX];
+
+  var startKey = `${startX},${startY}`;
+  var seenPoints = { [startKey]: true };
+
+  function getMatchingNeighbors(currentX, currentY) {
+    return neighboringPoints(grid, currentX, currentY).reduce((matches, point) => {
+      var [x, y] = point;
+      var key = point.join(',');
+
+      var isNewMatch = grid[y][x] === targetValue && !(key in seenPoints);
+      if (isNewMatch) {
+        seenPoints[key] = true;
+        return matches.concat([point], getMatchingNeighbors(x, y));
+      }
+      return matches;
+    }, []);
+  }
+
+  return getMatchingNeighbors(startX, startY);
+}
+
+function neighboringPoints(grid, x, y) {
+  var width  = grid[0].length;
+  var height = grid.length;
+  return [
+    [x - 1, y], [x, y - 1],
+    [x + 1, y], [x, y + 1],
+  ].filter(p => (
+    p[0] >= 0 && p[0] < width &&
+    p[1] >= 0 && p[1] < height
+  ));
+}
 
 function range(size, valueOrFn) {
   var fn = typeof valueOrFn === 'function' ? valueOrFn : (()=> valueOrFn);
